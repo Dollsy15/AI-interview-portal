a2e7f4d4a18ed23e17c53dbe474793392f443147
Dollsy Rani <dollsyrani155@gmail.com>
2025-07-18 12:34:35 +0530

    Changes going on

--- AI-interview-portal/src/pages/LandingPage.jsx
+++ AI-interview-portal/src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ const Landing = ({ onLoginClick }) => {
   return (
     <div className="landing-container">
       <div className="landing-content">
-        <h1>Welcome to AI Interview Portal</h1>
+        <h1>Welcome to <br/> AI Interview Portal</h1>
         <p>Scroll down to explore. </p>
         <button onClick={onLoginClick}>Click here to Login</button>
       </div>
